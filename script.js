1daa1f4ba70125ab8086671bd51dff27d331c242
kamal-stark-dev <kamalveer356@gmail.com>
2025-10-25 17:53:32 +0530

    add rendering of blocks, score and gameover screen

--- script.js
+++ script.js
@@ -21,8 +21,8 @@ const SHAPES = [
   ],
   // O
   [
-    [0, 0],
-    [0, 0],
+    [1, 1],
+    [1, 1],
   ],
   // S
   [
@@ -58,7 +58,7 @@ const SHAPE_COLORS = [
 
 const COLOR_SIDEBAR_BORDER = "#DDD";
 const COLOR_EMPTY_BLOCK = "#343434";
-const COLOR_GAME_OVERLAY = "#000000BB";
+const COLOR_GAME_OVER_OVERLAY = "#000000BB";
 const COLOR_FONT = "#FFF";
 
 const BLOCK_SIZE = 26;
@@ -96,6 +96,7 @@ const CANVAS_WIDTH = GRID_WIDTH + SIDEBAR_BORDER + SIDEBAR_WIDTH;
 const CANVAS_HEIGHT = GRID_HEIGHT;
 
 const BLOCK_EMPTY = -1;
+const MAX_DT = 100; // setting the maximum limit for delta-time
 
 function initCanvas() {
   const canvas = document.getElementById("game");
@@ -126,7 +127,7 @@ function createCurrentPiece(shapeId) {
 
   return {
     shapeId,
-    shape: [],
+    shape: SHAPES[shapeId],
     position: {
       x: getRandomIndex(GRID_COLS - shape[0].length + 1),
       y: 0,
@@ -145,16 +146,102 @@ function getInitialState() {
       speed: GRAVITY_SPEED,
     },
     currentPiece: createCurrentPiece(initialShapeId),
-    nextShapeId: getRandomShapeId,
+    nextShapeId: getRandomShapeId(),
     grid: makeEmptyGrid(),
   };
+}
+
+function update(state, inputs, dt) {}
+
+function drawBlock(ctx, color, x, y) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x + 1, y + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
+}
+
+function drawShape(ctx, shape, colorId, x, y) {
+  const color = SHAPE_COLORS[colorId];
+
+  for (i = 0; i < shape.length; i++) {
+    for (let j = 0; j < shape[0].length; j++) {
+      if (shape[i][j]) {
+        drawBlock(ctx, color, x + j * BLOCK_SIZE, y + i * BLOCK_SIZE);
+      }
+    }
+  }
+}
+
+function render(ctx, state) {
+  ctx.fillStyle = BLOCK_BACKGROUND;
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+  const { grid, currentPiece, nextShapeId } = state;
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      const colorId = grid[i][j];
+
+      const color =
+        colorId === BLOCK_EMPTY ? COLOR_EMPTY_BLOCK : SHAPE_COLORS[colorId];
+
+      drawBlock(ctx, color, j * BLOCK_SIZE, i * BLOCK_SIZE);
+    }
+  }
+
+  drawShape(
+    ctx,
+    currentPiece.shape,
+    currentPiece.shapeId,
+    currentPiece.position.x * BLOCK_SIZE,
+    currentPiece.position.y * BLOCK_SIZE
+  );
+
+  drawShape(
+    ctx,
+    SHAPES[nextShapeId],
+    nextShapeId,
+    SIDEBAR_CONTENT_X,
+    BLOCK_SIZE
+  );
+
+  ctx.fillStyle = COLOR_SIDEBAR_BORDER;
+  ctx.fillRect(GRID_WIDTH, 0, SIDEBAR_BORDER, CANVAS_HEIGHT);
+
+  ctx.font = "bold 24px monospace";
+  ctx.fillStyle = COLOR_FONT;
+  ctx.textAlign = "left";
+  ctx.textBaseline = "top";
+
+  const score = `${state.score}`.padStart(7, "0");
+  ctx.fillText("Score:", SIDEBAR_CONTENT_X, SIDEBAR_CONTENT_Y + BLOCK_SIZE * 5);
+  ctx.fillText(score, SIDEBAR_CONTENT_X, SIDEBAR_CONTENT_Y + BLOCK_SIZE * 6);
+
+  if (state.isGameOver) {
+    ctx.fillStyle = COLOR_GAME_OVER_OVERLAY;
+    ctx.fillRect(0, 0, GRID_WIDTH, GRID_HEIGHT);
+
+    ctx.fillStyle = COLOR_FONT;
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText("Game Over!", GRID_WIDTH / 2, GRID_HEIGHT / 2);
+  }
 }
 
 function main() {
   const ctx = initCanvas();
   const state = getInitialState();
+  const inputs = {};
 
-  console.log(state);
+  let previousTime = performance.now();
+
+  function loop(currentTime) {
+    const dt = Math.min(currentTime - previousTime, MAX_DT);
+    previousTime = currentTime;
+
+    update(state, inputs, dt);
+    render(ctx, state);
+    requestAnimationFrame(loop);
+  }
+  requestAnimationFrame(loop);
 }
 
 main();
