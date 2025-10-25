3cba0f47248ef50ee8c9b5677718e4180e8ad5dd
kamal-stark-dev <kamalveer356@gmail.com>
2025-10-25 19:53:23 +0530

    add tetris MVP

--- script.js
+++ script.js
@@ -74,6 +74,11 @@ const GRID_ROWS = 20;
 const SIDEBAR_BORDER = 20;
 const SIDEBAR_WIDTH_BLOCKS = 6;
 
+const MAX_DT = 100; // setting the maximum limit for delta-time
+
+const INPUT_REPEAT_THRESHOLD = 400;
+const INPUT_REPEAT_INTERVAL = 5;
+
 const KEY_TO_INPUT_TYPE = {
   ArrowLeft: "moveLeft",
   ArrowRight: "moveRight",
@@ -96,7 +101,10 @@ const CANVAS_WIDTH = GRID_WIDTH + SIDEBAR_BORDER + SIDEBAR_WIDTH;
 const CANVAS_HEIGHT = GRID_HEIGHT;
 
 const BLOCK_EMPTY = -1;
-const MAX_DT = 100; // setting the maximum limit for delta-time
+
+const INPUT_STATE_INITIAL = 0;
+const INPUT_STATE_CHARGING = 1;
+const INPUT_STATE_REPEATING = 2;
 
 function initCanvas() {
   const canvas = document.getElementById("game");
@@ -127,7 +135,7 @@ function createCurrentPiece(shapeId) {
 
   return {
     shapeId,
-    shape: SHAPES[shapeId],
+    shape: SHAPES[shapeId].map((row) => [...row]),
     position: {
       x: getRandomIndex(GRID_COLS - shape[0].length + 1),
       y: 0,
@@ -187,35 +195,159 @@ function moveCurrentPiece(grid, currentPiece, moveX, moveY) {
   return canMove;
 }
 
-function updateCurrentPiece(state, inputs, dt) {
-  const { grid, currentPiece } = state;
+function rotate(shape) {
+  return Array.from({ length: shape[0].length }, (_, i) =>
+    Array.from(
+      { length: shape.length },
+      (_, j) => shape[shape.length - 1 - j][i]
+    )
+  );
+}
 
-  if (inputs.moveLeft) {
-    // move left
-    moveCurrentPiece(grid, currentPiece, -1, 0);
-  }
-  if (inputs.moveRight) {
-    // move right
-    moveCurrentPiece(grid, currentPiece, 1, 0);
-  }
-  if (inputs.moveDown) {
-    // move down
-    moveCurrentPiece(grid, currentPiece, 0, 1);
-  }
-  if (inputs.rotate) {
-    // rotate
-  }
-  if (inputs.hardDrop) {
-    // hard drop
+function rotateCurrentPiece(grid, currentPiece) {
+  const { shape, position } = currentPiece;
+
+  const newShape = rotate(shape);
+
+  if (canGridFitShape(grid, newShape, position.x, position.y)) {
+    currentPiece.shape = newShape;
   }
 }
 
-function updateGravity(state, dt) {}
+function handleInputState(input, dt) {
+  if (!input) return false;
+
+  input.timer += dt;
+
+  switch (input.state) {
+    case INPUT_STATE_INITIAL:
+      input.state = INPUT_STATE_CHARGING;
+      return true;
+
+    case INPUT_STATE_CHARGING:
+      const isCharged = input.timer >= INPUT_REPEAT_THRESHOLD;
+      if (isCharged) {
+        input.state = INPUT_STATE_REPEATING;
+        input.timer = 0;
+      }
+
+      return isCharged;
+
+    case INPUT_STATE_REPEATING:
+      const shouldRepeat = input.timer >= INPUT_REPEAT_INTERVAL;
+      if (shouldRepeat) {
+        input.timer = 0;
+      }
+      return shouldRepeat;
+  }
+}
+
+function updateCurrentPiece(state, inputs, dt) {
+  const { grid, currentPiece } = state;
+
+  const isInputActive = (inputType) => handleInputState(inputs[inputType], dt);
+
+  if (isInputActive("moveLeft")) {
+    moveCurrentPiece(grid, currentPiece, -1, 0);
+  }
+  if (isInputActive("moveRight")) {
+    moveCurrentPiece(grid, currentPiece, 1, 0);
+  }
+  if (isInputActive("moveDown")) {
+    // moveCurrentPiece(grid, currentPiece, 0, 1);
+    moveCurrentPieceDown(state);
+  }
+  if (isInputActive("rotate")) {
+    rotateCurrentPiece(grid, currentPiece);
+  }
+  if (isInputActive("hardDrop")) {
+    // hard drop
+    while (moveCurrentPieceDown(state)) {}
+  }
+}
+
+function attachToGrid(grid, currentPiece) {
+  const { shapeId, shape, position } = currentPiece;
+
+  for (let i = 0; i < shape.length; i++) {
+    for (let j = 0; j < shape[0].length; j++) {
+      if (shape[i][j]) {
+        grid[position.y + i][position.x + j] = shapeId;
+      }
+    }
+  }
+}
+
+function clearCompleteLines(grid) {
+  let clearedLines = 0;
+
+  for (let i = grid.length - 1; i >= 0; i--) {
+    if (grid[i].every((cell) => cell !== BLOCK_EMPTY)) {
+      clearedLines++;
+    } else if (clearedLines > 0) {
+      grid[i + clearedLines] = [...grid[i]];
+    }
+  }
+
+  // clear top rows
+  for (let i = 0; i < clearedLines; i++) {
+    grid[i].fill(BLOCK_EMPTY);
+  }
+
+  return clearedLines;
+}
+
+function handleCurrentPieceLanding(state) {
+  // attach shape to the grid
+  attachToGrid(state.grid, state.currentPiece);
+
+  // clear complete lines
+  const clearedLines = clearCompleteLines(state.grid);
+
+  // update score
+  state.score += clearedLines;
+
+  // create new piece
+  const newPiece = createCurrentPiece(state.nextShapeId);
+  const { shape, position } = newPiece;
+
+  // if it doesn't fit - game over
+  if (canGridFitShape(state.grid, shape, position.x, position.y)) {
+    state.currentPiece = newPiece;
+    state.nextShapeId = getRandomShapeId();
+  } else {
+    state.isGameOver = true;
+  }
+}
+
+function moveCurrentPieceDown(state) {
+  state.gravity.progress = 0;
+
+  const didMove = moveCurrentPiece(state.grid, state.currentPiece, 0, 1);
+  if (!didMove) {
+    // handle landing
+    handleCurrentPieceLanding(state);
+  }
+  return didMove;
+}
+
+function updateGravity(state, dt) {
+  state.gravity.speed += GRAVITY_ACCELERATION * dt;
+  state.gravity.progress += state.gravity.speed * dt;
+
+  if (state.gravity.progress >= GRAVITY_THRESHOLD) {
+    moveCurrentPieceDown(state);
+  }
+}
+
+function resetGameState(state) {
+  Object.assign(state, getInitialState());
+}
 
 function update(state, inputs, dt) {
   if (state.isGameOver) {
     if (inputs.restart) {
-      // restart the game
+      resetGameState(state);
     }
   } else {
     // main gameplay
@@ -308,8 +440,10 @@ function startCollectingInputs(inputs) {
     }
   }
 
-  window.addEventListener("keydown", (event) => handleKeyEvent(event, true));
-  window.addEventListener("keyup", (event) => handleKeyEvent(event, false));
+  window.addEventListener("keydown", (event) =>
+    handleKeyEvent(event, { state: INPUT_STATE_INITIAL, timer: 0 })
+  );
+  window.addEventListener("keyup", (event) => handleKeyEvent(event, undefined));
 }
 
 function main() {
